d2d9d182ba9e373da2eeb55b255ba6ec6c8eb07c
Daniel Borges <daniel0107borges@gmail.com>
2024-05-12 22:13:53 -0300

    classeDoPokemon

---
+++ src/main/java/com/app/pokedex/model/Pokemon.java
@@ -0,0 +1,16 @@
+package com.app.pokedex.model;
+
+public class Pokemon {
+
+    public String nome; 
+    public String tipo;
+    public int id; 
+
+
+public Pokemon(String nome, String tipo){
+    this.nome = nome;
+    this.tipo = tipo;
+    this.id = id;
+}
+
+}
