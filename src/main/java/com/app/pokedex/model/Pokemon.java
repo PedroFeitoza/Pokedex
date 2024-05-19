1e8f4b2d59aef0a44f4b9e81ed8db61a7f0f79ff
Daniel Borges <daniel0107borges@gmail.com>
2024-05-18 21:53:43 -0300

    testeGetterSetter

--- src/main/java/com/app/pokedex/model/Pokemon.java
+++ src/main/java/com/app/pokedex/model/Pokemon.java
@@ -2,9 +2,38 @@ package com.app.pokedex.model;
 
 public class Pokemon {
 
-    public String nome; 
+    private String nome; 
     public String tipo;
-    public int id; 
+    private int id; 
+    //getter e setter ID 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int PokeId) {
+        id = PokeId;
+    }
+
+    //getter e setter Nome 
+    public void setNome(String meuNome){
+        nome = meuNome;
+        
+    }
+
+    public String getNome() {
+        return nome; 
+    }
+
+
+    //MOSTRAR NOME 
+    public void mostraNome() {
+        System.out.printf("O nome do seu pokemon é %s \n ", getNome());
+
+    public void mostrarId() {
+        System.out.printf("O ID do seu pokemon é %s \n", getId());
+       
+    }
+
 
 
 public Pokemon(String nome, String tipo){
