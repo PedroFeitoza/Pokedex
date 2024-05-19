2c791d4400ed982ae5b30a7c3f73a454ccc22a12
Daniel Borges <daniel0107borges@gmail.com>
2024-05-19 16:43:40 -0300

    alteração no service

--- src/main/java/com/app/pokedex/service/PokemonService.java
+++ src/main/java/com/app/pokedex/service/PokemonService.java
@@ -1,6 +1,10 @@
 package com.app.pokedex.service;
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.app.pokedex.model.ExemploModel;
 import com.app.pokedex.model.Pokemon;
 import com.app.pokedex.service.base.FileService;
 
@@ -16,8 +20,15 @@ public class PokemonService extends FileService<Pokemon>{
     @Override
     protected Class<Pokemon> getClassModel() {
         return Pokemon.class;
-
        
+    }
+    public List<Pokemon> getAll() throws IOException {
+        return this.getAllFromFile();
+    }
+
+        public  Pokemon getById(Long id) throws IOException {
+            return this.getItemByIdFromFile(id);
+
     }
     
 
