6ed733f197007563a8b99ee8eca2af4100ae110e
Daniel Borges <daniel0107borges@gmail.com>
2024-05-19 23:06:26 -0300

    ajuste final service pokemon

--- src/main/java/com/app/pokedex/service/PokemonService.java
+++ src/main/java/com/app/pokedex/service/PokemonService.java
@@ -1,16 +1,17 @@
 package com.app.pokedex.service;
+
 import java.io.IOException;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.app.pokedex.model.ExemploModel;
 import com.app.pokedex.model.Pokemon;
 import com.app.pokedex.service.base.FileService;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 
 @Service
-public class PokemonService extends FileService<Pokemon>{
-
+public class PokemonService extends FileService<Pokemon> {
 
     @Override
     protected String getFileName() {
@@ -20,17 +21,21 @@ public class PokemonService extends FileService<Pokemon>{
     @Override
     protected Class<Pokemon> getClassModel() {
         return Pokemon.class;
-       
+
     }
+
     public List<Pokemon> getAll() throws IOException {
         return this.getAllFromFile();
     }
 
-        public  Pokemon getById(Long id) throws IOException {
-            return this.getItemByIdFromFile(id);
+    public Pokemon getById(Long id) throws IOException {
+        return this.getItemByIdFromFile(id);
 
     }
-    
 
+    public Pokemon saveItem(Pokemon e)
+            throws CsvDataTypeMismatchException, CsvRequiredFieldEmptyException, IOException {
+        return this.saveItemInFile(e);
+    }
 
 }
